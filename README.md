3a1354005122f7b9f2fa3e3612eac320be81c212
TCizzle01 <77759320+TCizzle01@users.noreply.github.com>
2021-03-31 19:23:06 -0400

    update afternoon 3/31

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Final Project
 
 This is the final project for the Code Louisville Front End Web Development Class. This site is a template for an interactive resume'. 
-Clicking on each button reveals a different element of a resume.  
+Clicking on each button reveals a different element of a resume. 
+When the the viewwer becomes skinny the tabs change to a formation fit for a vertical view. 
+When the view changes to landscape and the screen is still small the tabs move over to the right so the name may still be displayed. 
+
+CSS feaatures: A nth child on the skills section highlights every other list item for easier reading. The job and school options are in a grid format. 
--- myScript.js
+++ myScript.js
@@ -3,10 +3,27 @@ var btn = document.getElementsByClassName("collapse");
         btn[0].addEventListener("click", function () {
             this.classList.toggle("active");
             var content = this.nextElementSibling;
-            if (content.style.display === "block") {
+            if (content.style.display === "grid") {
                 content.style.display = "none";
             } else {
-                content.style.display = "block";
+                content.style.display = "grid";
             }
         });
-  
+        btn[1].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "grid") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "grid";
+            }
+        });
+        btn[2].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "list") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "list";
+            }
+        });
