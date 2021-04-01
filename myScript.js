bde73919d2720bf5a77264f0ff6c144c0b22dac7
TCizzle01 <77759320+TCizzle01@users.noreply.github.com>
2021-03-31 20:17:23 -0400

    grid

--- myScript.js
+++ myScript.js
@@ -21,9 +21,9 @@ var btn = document.getElementsByClassName("collapse");
         btn[2].addEventListener("click", function () {
             this.classList.toggle("active");
             var content = this.nextElementSibling;
-            if (content.style.display === "list") {
+            if (content.style.display === "grid") {
                 content.style.display = "none";
             } else {
-                content.style.display = "list";
+                content.style.display = "grid";
             }
         });
